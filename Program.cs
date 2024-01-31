d1d38d9ced047fda2cfcffd43e7ea247b72f9ec7
NayabAkhtar12 <119120061+NayabAkhtar12@users.noreply.github.com>
2024-01-31 17:15:00 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace C_Sharp_Daily
+{
+
+    internal class Program
+    {
+        public static int Factorial(int num)
+        {
+            int Fact = 1;
+            for(int i = 1; i <= num; i++) 
+            {
+                Fact = Fact * i;
+            }
+            return Fact;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int Number=int.Parse(Console.ReadLine());
+            Console.WriteLine($"Factorial of a Number is\t{Factorial(Number)}");
+        }
+    }
+}
